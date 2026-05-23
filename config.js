d6a4533120cdfe5dd0351a6a357b2df88c1eeac3
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:09:42 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THE AMAZING SPIDER-MAN';
+export const nameSongs = [
+	"THEME", /*1*/
+	"THERE HE IS", /*2*/
+	"I'M SPIDER-MAN", /*3*/
+	"GROUND RULES", /*4*/
+	"LOOK AT ME", /*5*/
+	"SPECIAL PROJECT", /*6*/
+	"YOU NEED ME", /*7*/
+	"SO MUCH ANGER", /*8*/
+	"I'M MOVING TO ENGLAND", /*9*/
+	"I'M GOBLIN", /*10*/
+	"YOU'RE MY BOY", /*11*/
+	"I NEED TO KNOW", /*12*/
+	"WE'RE BEST FRIENDS", /*13*/
+	"STILL CRAZY", /*14*/
+	"THE REST OF MY LIFE", /*15*/
+	"YOU'RE THAT SPIDER GUY", /*16*/
+	"COLD WAR", /*17*/
+	"NO PLACE LIKE HOME", /*18*/
+	"IT'S ON AGAIN", /*19*/
+	"ELECTRO REMIX", /*20*/
+]
